0c97dfb64fb8b74872692c7ce0681bbb5dcd7d40
Elizaveta Nasytko <Liz99246@gmail.com>
2024-09-14 21:57:43 +0300

    classes and tests are done i guess

---
+++ src/classes.py
@@ -0,0 +1,28 @@
+class Category:
+    category_count = 0
+    product_count = 0
+
+    name: str
+    description: str
+    products: str
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
+
+
+class Product:
+    name: str
+    description: str
+    price: int
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
